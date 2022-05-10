19387ced2a95acaa0aa7dfa3dd068b8934feba5f
Jonas Vasconcelos <jonas.vasconcelos.filho@gmail.com>
2022-05-10 09:17:53 -0300

    v1

---
+++ script.js
@@ -0,0 +1,73 @@
+let dados = [
+            {
+                nome: 'Jonas',
+                senha: '1234',
+                cargo: 'funcionario'
+            },
+            {
+                nome: 'JonasVas',
+                senha: '123456',
+                cargo: 'adm'
+            }
+]
+
+const inputFields = document.querySelectorAll('input');
+const loginInput = document.getElementById('login');
+const passwordInput = document.getElementById('password');
+const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnlogout');
+const mainTables = document.getElementsByClassName('container');
+
+const erro = document.getElementById('erro');
+
+inputFields.forEach(function(inputItem){
+  inputItem.addEventListener('input', function() {
+    if (passwordInput.value.length > 3 & loginInput.value.length > 3) {
+      btnLogin.classList.remove('disabled');
+    } else {
+      btnLogin.classList.add('disabled');
+    }
+  });
+});
+
+function validation() {
+  let flag = false;
+  for (let i = 0; i < dados.length; i += 1) {
+    if (loginInput.value === dados[i].nome && passwordInput.value === dados[i].senha){
+      flag = true;
+      open('./tables.html', target = '_self')
+      break;
+    }
+  }
+  if (flag === false){
+    var e = new Error('Could not parse input');
+    throw e;
+  }
+};
+
+function createTable(N) {
+  for(let i = 0; i < N ; i += 1) {
+    let table = document.createElement('div');
+    table.classList.add('table')
+    mainTables[0].appendChild(table);
+  }
+}
+
+
+btnLogin.addEventListener('click', validation);
+window.addEventListener('keydown', function (event) {
+  if (event.defaultPrevented) {
+    return; 
+  }
+  switch (event.key) {
+    case "Enter":
+      validation();
+      break;
+    default:
+      return; 
+  }
+  event.preventDefault();
+}, true);
+
+
+window.onload = createTable(10);
---
+++ scriptTable.js
@@ -0,0 +1,42 @@
+let dados = [
+    {
+        nome: 'Jonas',
+        senha: '1234',
+        cargo: 'funcionario'
+    },
+    {
+        nome: 'JonasVas',
+        senha: '123456',
+        cargo: 'adm'
+    }
+]
+
+const inputFields = document.querySelectorAll('input');
+const loginInput = document.getElementById('login');
+const passwordInput = document.getElementById('password');
+const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnlogout');
+const mainTables = document.getElementsByClassName('container');
+
+function createTable(N) {
+  for(let i = 0; i < N ; i += 1) {
+  let table = document.createElement('div');
+  table.classList.add('table')
+  table.innerHTML = i;
+  mainTables[0].appendChild(table);
+  }
+  picker();
+}
+
+function picker() {
+  document.querySelectorAll('.table').forEach((item) => {
+    item.addEventListener("click", (event) => {
+      /* event.target.classList.add('busy'); */
+      
+      open('./comanda.html', '_self')
+    })
+  })
+}
+
+
+window.onload = createTable(10);
