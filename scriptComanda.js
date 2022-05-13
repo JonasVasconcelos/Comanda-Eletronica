40da065b154eea9fe003d045edff61d326554a8f
Jonas Vasconcelos <jonas.vasconcelos.filho@gmail.com>
2022-05-13 14:01:18 -0300

    comanda

--- scriptComanda.js
+++ scriptComanda.js
@@ -3,11 +3,16 @@ const date = today.getDate() + '/' + (today.getMonth()+1) + '/' + today.getFullY
 const mesaCurrent = localStorage.getItem('mesa')
 const garcomCurrent = localStorage.getItem('garcom')
 
+const btnProx = document.getElementById('pedidosProx')
+
 const dataComanda = document.getElementById('data');
 const mesaComanda = document.getElementById('mesa');
 const garcomComanda = document.getElementById('garcom');
 
-
 dataComanda.innerHTML = date;
 mesaComanda.innerHTML += mesaCurrent;
 garcomComanda.innerHTML += garcomCurrent;
+
+btnProx.addEventListener('click', () =>{
+    open('./Resumo.html', target = '_self')
+})
