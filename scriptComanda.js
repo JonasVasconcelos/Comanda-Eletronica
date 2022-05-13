1885d44121875d2a33ddf4bad282d2e11f8963c6
Jonas Vasconcelos <jonas.vasconcelos.filho@gmail.com>
2022-05-13 19:21:10 -0300

    comanda v2

--- scriptComanda.js
+++ scriptComanda.js
@@ -3,7 +3,7 @@ const date = today.getDate() + '/' + (today.getMonth()+1) + '/' + today.getFullY
 const mesaCurrent = localStorage.getItem('mesa')
 const garcomCurrent = localStorage.getItem('garcom')
 
-const btnProx = document.getElementById('pedidosProx')
+const btnProx = document.getElementsByClassName('pedidosProx')
 
 const dataComanda = document.getElementById('data');
 const mesaComanda = document.getElementById('mesa');
@@ -13,6 +13,85 @@ dataComanda.innerHTML = date;
 mesaComanda.innerHTML += mesaCurrent;
 garcomComanda.innerHTML += garcomCurrent;
 
-btnProx.addEventListener('click', () =>{
-    open('./Resumo.html', target = '_self')
+let form = document.querySelector('form');
+let brahma600 = document.getElementById('brahma600');
+let brahma1000 = document.getElementById('brahma1000');
+let devassa600 = document.getElementById('devassa600');
+let heinekenlong = document.getElementById('heinekenlong');
+let heineken600 = document.getElementById('heineken600');
+let guarana = document.getElementById('guarana');
+
+let menu = {
+    brahma600: 5,
+    brahma1000: 5,
+    devassa600: 5,
+    heinekenlong: 5,
+    heineken600: 5,
+    guarana: 5
+}
+
+function createMenu(menu) {
+    this.table = mesaCurrent;
+    this.consumption = [];
+    this.qnt = [];
+    this.order = (string, number) => {
+        this.consumption.push(string);
+        this.qnt.push(number);
+    };
+    this.pay = () => {
+      let sum = 0;
+      for (let i = 0; i < this.consumption.length; i += 1) {
+        let index = menu[this.consumption[i]];
+        sum += index;
+      }
+      return sum;
+    };
+}
+
+let Order = new createMenu(menu)
+const Inputs = document.querySelectorAll('input')
+const Labels = document.querySelectorAll('label')
+
+function addPedido() {
+  let N = Inputs.length;
+  for(i = 0; i < N; i++) {
+    if(Number(Inputs[i].value) > 0) {
+      Order.order(Labels[i].innerHTML, Number(Inputs[i].value));
+    }
+  }
+}
+
+
+function fillFormWithUserInfo() {
+    addPedido();
+    form.style.display = 'block';
+    form.style.paddingTop = '10px';
+    form.innerHTML = `<h2>Resumo do pedido</h2>`
+    form.innerHTML += `<h3>Mesa: ${Order.table}</h3>`
+    for(i = 0; i < Order.consumption.length; i++){
+        form.innerHTML += `<p class='pResumo'> ${Order.qnt[i]} ${Order.consumption[i].replace(':','')}</p>`
+    }
+    
+    let diV = document.createElement('div');
+    diV.classList.add('divResumo');
+    form.appendChild(diV);
+
+    let btn = document.createElement('button');
+    btn.classList.add('pedidosReset');
+    btn.style.width = '50%';
+    btn.style.color = 'white';
+    btn.innerHTML = 'Apagar tudo?'
+    diV.appendChild(btn);
+
+    btn = document.createElement('button');
+    btn.classList.add('pedidosProx');
+    btn.style.width = '50%';
+    btn.style.color = 'white';
+    btn.innerHTML = 'Confirmar?'
+    diV.appendChild(btn);
+}
+
+btnProx[0].addEventListener('click', () =>{
+    /* open('./Resumo.html', target = '_self') */
+    fillFormWithUserInfo();
 })
