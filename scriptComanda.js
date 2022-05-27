e7a59af19b258eca9973dc532f4f0cef209f34e4
Jonas Vasconcelos <jonas.vasconcelos.filho@gmail.com>
2022-05-27 17:06:25 -0300

    pagou a conta

--- scriptComanda.js
+++ scriptComanda.js
@@ -10,6 +10,8 @@ const mesaComanda = document.getElementById('mesa');
 const garcomComanda = document.getElementById('garcom');
 
 const totalDaConta = document.getElementById('totalDaConta');
+const obsData = document.getElementById('obs')
+const btnFecharConta = document.getElementsByClassName('fecharComanda');
 
 const getDados = (name) => JSON.parse(localStorage.getItem(name)) ?? []
 const setDados = (name, obj) => localStorage.setItem(name, JSON.stringify(obj))
@@ -70,6 +72,7 @@ function fillFormWithClientOrder() {
     for(i = 0; i < Order.consumption.length; i++){
         form.innerHTML += `<p class='pResumo'> ${Order.qnt[i]} ${Order.consumption[i]}</p>`
     }
+    form.innerHTML += `<p class='pResumo'> ${obsData.value}</p>`
     
     let diV = document.createElement('div');
     diV.classList.add('divResumo');
@@ -144,3 +147,54 @@ const totalUpdate = (mesaCurrent) => {
 }
 
 totalDaConta.innerHTML = `Total: R$ ${totalUpdate(mesaCurrent)}`
+
+const pagouConta = () => {
+  localStorage.removeItem('mesa' + mesaCurrent)
+  let tableBusy = getDados('tableBusy')
+  for(i in tableBusy){
+    console.log(tableBusy[i])
+    if(tableBusy[i] === mesaCurrent){
+      tableBusy.splice(i, 1)
+    }
+  }
+  setDados('tableBusy', tableBusy)
+  open('./tables.htlm', '_self')
+}
+
+const fecharConta = () => {
+  const orders = getDados('mesa' + mesaCurrent);
+  let res = orders.reduce((acc, curr) => {
+    if (curr in acc) acc[curr]++;
+    else acc[curr] = 1;
+    return acc;
+  }, {});
+  let preco = Object.keys(res).map((item) => res[item] * menu[item]);
+  
+  const resultado = ({
+                    qnt: Object.values(res),
+                    item: Object.keys(res),
+                    preco: preco,
+                    })
+
+  form.classList.add('formContaFinal')
+  form.innerHTML = `<h2 class = 'infoContaFinal'>Resumo do pedido</h2>`
+  form.innerHTML += `<h3 class = 'infoContaFinal'>Mesa: ${Order.table}</h3>`
+  form.innerHTML += `<p class = 'pContaFinal qntContaFinal rotuloContaFinal'> Qnt.</p> <p class = 'pContaFinal itemContaFinal rotuloContaFinal'>Descrição </p> <p class = 'pContaFinal precoContafinal rotuloContaFinal'> Preço (R$) </p>`
+  for(i = 0; i < resultado.qnt.length; i++){
+      form.innerHTML += `<p class = 'pContaFinal qntContaFinal'>${resultado.qnt[i]}</p><p class = 'pContaFinal itemContaFinal'>${resultado.item[i]}</p><p class = 'pContaFinal precoContafinal'>${resultado.preco[i].toFixed(2)}</p>`
+  }
+  
+  form.innerHTML += `<p class = 'pContaFinal qntContaFinal'> </p> <p class = 'pContaFinal precoContafinal totalContaFinal'>Total: </p> <p class = 'pContaFinal precoContafinal totalContaFinal'> ${totalUpdate(mesaCurrent)} </p>`
+
+  btn = document.createElement('button');
+  btn.classList.add('pedidosProx');
+  btn.classList.add('confirmar');
+  btn.setAttribute('id', 'Confirmar')
+  btn.style.width = '100%';
+  btn.style.color = 'white';
+  btn.innerHTML = 'Confirmar?'
+  form.appendChild(btn);
+  btn.addEventListener('click', pagouConta)
+}
+
+btnFecharConta[0].addEventListener('click', fecharConta)
